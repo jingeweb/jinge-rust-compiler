634d04e4eff4d9b47bff2e84d4f566f361e82338
yuhang <abeyuhang@gmail.com>
2024-08-14 22:25:04 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # jinge swc compiler
 
+## Develop
+
+### 环境准备
+
+注意：当前暂时仅支持在 mac 或 windows 下构建（交叉编译构建 linux 和 windows 平台）。
+
+TODO: 使用 Github Actions 跨平台构建并自动发布 npm 包。
+
+准备环境：
+
+1. 安装 `rust` 和 `node` 环境。
+2. 安装 `rust` 交叉编译的环境。
+
+   - `mac` 平台下：
+
+   ```bash
+   # windows 交叉编译
+   rustup target add x86_64-pc-windows-gnu
+   brew install mingw-w64
+   # linux 交叉编译
+   rustup target add x86_64-unknown-linux-musl
+   brew install FiloSottile/musl-cross/musl-cross
+   ```
+
 ## TODO
 
 - Related Component 的完善和测试
--- ts/binding.ts
+++ ts/binding.ts
@@ -1,5 +1,5 @@
 import { createRequire } from 'module';
-
+import os from 'os';
 export interface TransformOptions {
   sourcemap?: boolean;
 }
@@ -15,7 +15,22 @@ export interface JingeCompiler {
   };
 }
 
+function getBinding() {
+  const platform = os.platform();
+
+  const arch = os.arch();
+  if (platform === 'darwin') {
+    return arch === 'arm64' ? 'macos-aarch64.node' : 'macos-x86_64.node';
+  } else if (platform === 'win32') {
+    if (arch === 'x64') return 'windows-x86_64.node';
+  } else if (platform === 'linux') {
+    if (arch === 'x64') return 'linux-x86_64.node';
+    else if (arch === 'arm64') return 'linux-aarch64.node';
+  }
+  throw new Error(`unsupport platform ${platform}-${arch}`);
+}
 export function loadBinding(debug = true) {
   const require = createRequire(import.meta.url);
-  return require(`../index${debug ? '.debug' : ''}.node`) as JingeCompiler;
+  if (debug) return require('../index.debug.node') as JingeCompiler;
+  return require(`jinge-compiler-core-${getBinding()}`) as JingeCompiler;
 }
